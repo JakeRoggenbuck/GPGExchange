c84ca30cef3bfa069744b58648073b2efb3dd4c5
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-08-18 15:06:18 -0700

    Use database for file peculator

--- gpg_exchange.py
+++ gpg_exchange.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+from database import DataBase
 
 
 UPLOAD_FOLDER = 'files/'
@@ -9,7 +10,7 @@ ALLOWED_EXTENSIONS = {'asc', 'pgp', 'gpg'}
 GPG_EXCHANGE = Flask(__name__)
 GPG_EXCHANGE.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-FILES = []
+DATABASE = DataBase()
 
 
 def allowed_file(filename):
@@ -17,36 +18,33 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def get_data(title, description, email, filename):
-    file_entry = (title, description, email, filename)
-    FILES.append(file_entry)
-
-
 @GPG_EXCHANGE.route('/')
 def home():
-    get_data("title1", "desc1", "email@mail.com", "file.txt")
-    get_data("title2", "desc2", "boomer@mail.com", "test.txt")
-    return render_template("home.html", files=FILES)
+    # Pulls all data
+    all_data = DATABASE.read_all()
+    # Sends all data to template as a list of tuples
+    return render_template("home.html", files=all_data)
 
 
 @GPG_EXCHANGE.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
+        # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
+        # If user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(GPG_EXCHANGE.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file',
-                                    filename=filename))
+            file_path = GPG_EXCHANGE.config['UPLOAD_FOLDER']
+            full_path = os.path.join(file_path, filename)
+            file.save(full_path)
+            return redirect(url_for('upload_file', filename=filename))
 
     return render_template("upload.html")
 
